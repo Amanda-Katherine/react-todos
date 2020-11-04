c67cf51374c2cd72b4803f7abf6b8a200f69fd4f
Annabel Wilmerding <awilmerding@Annabel-Wilmerdings-MB-Pro.local>
2020-11-04 07:51:39 -0500

    intro to react

---
+++ src/List.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+class List extends Component {
+
+
+  render() {
+    return (
+      <ul>
+         {this.props.list.map(item => <li key={item}>{item}</li>)}
+      </ul>
+    )
+  }
+
+}
+
+export default List
+
+ 
