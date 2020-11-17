ef9b06f77ea5943a6e63921dd32a902fbac1e157
Annabel Wilmerding <awilmerding@AnnabelngsMBPro.cable.rcn.com>
2020-11-17 12:56:03 -0500

    adds redux

--- src/ListContainer.js
+++ src/ListContainer.js
@@ -3,64 +3,71 @@ import Lists from './Lists'
 import List from './List'
 import ListForm from './ListForm'
 import {Route, Switch} from 'react-router-dom'
+import {connect} from 'react-redux'
 
 class ListContainer extends PureComponent {
 
-  constructor(props) {
-      super(props)
-      this.state = {
-        lists: []
-      }
-      this.handleSubmit = this.handleSubmit.bind(this)
-    }
+  // constructor(props) {
+  //     super(props)
+  //     this.state = {
+  //       lists: []
+  //     }
+  //     // this.handleSubmit = this.handleSubmit.bind(this)
+  //   }
+
+    //
+    // componentDidMount() {
+    //   fetch("http://localhost:3000/lists")
+    //   .then(response => response.json())
+    //   .then(lists => {this.setState({lists: lists})})
+    //
+    // }
 
 
-    componentDidMount() {
-      fetch("http://localhost:3000/lists")
-      .then(response => response.json())
-      .then(lists => {this.setState({lists: lists})})
-
-    }
-
-
-    handleSubmit(formData){
-      const list = {name: formData.listName}
-      const options = {
-        method: "POST",
-        headers: {"Content-Type": "application/json", "Accept": "application/json"},
-        body: JSON.stringify({list})
-      }
-
-      fetch("http://localhost:3000/lists", options)
-      .then(r => r.json())
-      .then(list => {
-        this.setState((prevState) => {
-          return {lists: [...prevState.lists, list]}
-        })
-      })
-
-
-    }
+    // handleSubmit(formData){
+    //   const list = {name: formData.listName}
+    //   const options = {
+    //     method: "POST",
+    //     headers: {"Content-Type": "application/json", "Accept": "application/json"},
+    //     body: JSON.stringify({list})
+    //   }
+    //
+    //   fetch("http://localhost:3000/lists", options)
+    //   .then(r => r.json())
+    //   .then(list => {
+    //     this.setState((prevState) => {
+    //       return {lists: [...prevState.lists, list]}
+    //     })
+    //   })
+    //
+    //
+    // }
 
 
     render() {
-      console.log(this.state)
       return (
 
           <Switch>
               <Route exact path="/lists">
-                <Lists lists={this.state.lists}/>
+                <Lists lists={this.props.lists}/>
               </Route>
               <Route exact path="/lists/new">
                 <ListForm submitForm={this.handleSubmit}/>
               </Route>
-              <Route exact path="/lists/:id" render={(routerProps) => <List list={this.state.lists.find(list => list.id === parseInt(routerProps.match.params.id))}/>}/>
+              <Route exact path="/lists/:id" render={(routerProps) => <List {...routerProps } list={this.props.lists.find(list => list.id === parseInt(routerProps.match.params.id))} />}/>
             </Switch>
 
-          
+
       )
     }
 
 }
 
-export default ListContainer
+const mapStateToProps = state => {
+  
+    return {
+      lists: state.lists
+    }
+}
+
+export default connect(mapStateToProps)(ListContainer)
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
 import App from './App';
+import listReducer from './reducers/listReducer'
+
+const initialState = {lists: [{name: "Grocery", id: 1}, {name: "Work", id: 2}, {name: "Errands", id: 3}]}
+const store = createStore(listReducer, initialState, composeWithDevTools())
 
 ReactDOM.render(
   <Router>
+    <Provider store={store}>
       <App />
+    </Provider>
   </Router>,
   document.getElementById('root')
 );
